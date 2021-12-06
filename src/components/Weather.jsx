58a28c75e6427c97954341786d08ae46a6bae3eb
Auden Robertson <audenrobertson@gmail.com>
2021-12-06 16:44:56 -0500

    updating icons

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -16,29 +16,40 @@ const Weather = () => {
         await axios({
             method: 'GET',
             url:`http://api.openweathermap.org/data/2.5/weather?q=${city},
-            ${country}&appid={e8242f48fbfddf9af85e4c4ccc0394e0}`
-        }).then((res) => {
-            console.log(res.data.main.temp);
+            ${country}&appid={e8242f48fbfddf9af85e4c4ccc0394e0}`,
+        })
+          .then((res) => {
+            console.log(res.data);
             setTemp(res.data.main.temp);
-        }).catch((err) => {
+            setIcon(res.data.weather[0].icon);
+        })
+          .catch((err) => {
             console.log(err)
         })
     }
 
     return (
         <div className="container">
-            <input type="text" value={city} onChange={ 
-                (e) => setCity(e.target.value)}
-            placeholder="Cityname" 
+            <input 
+                type="text" 
+                value={city} 
+                onChange={(e) => setCity(e.target.value)}
+                placeholder="Cityname" 
             />
-            <input type="text" value={country} onChange={ 
-                (e) => setCountry(e.target.value)}
-            placeholder="Countryname" 
+            <input 
+                type="text" 
+                value={country} 
+                onChange={(e) => setCountry(e.target.value)}
+                placeholder="Countryname" 
             />
             <button onClick={getWeatherData(city, country)} className="btn btn-primary">Get Weather</button>
 
             <div className="data_container p-4 my-5">
                 <h1>{city}, {country}</h1>
+                <div className="my-2">
+                    <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} 
+                    alt="weather-icon" style={{width:200, height:200}}/>
+                </div>
             </div>
         </div>
     )
