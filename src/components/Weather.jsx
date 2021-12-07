5c665e013aac5b5d10ca9eb19a19a6c919a8dd89
Auden Robertson <audenrobertson@gmail.com>
2021-12-07 13:17:24 -0500

    setting up weather data

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -16,12 +16,17 @@ const Weather = () => {
         await axios({
             method: 'GET',
             url:`http://api.openweathermap.org/data/2.5/weather?q=${city},
-            ${country}&appid={e8242f48fbfddf9af85e4c4ccc0394e0}`,
+            ${country}&appid=689f9dd67eb2803392b2c876ffac4188`,
         })
           .then((res) => {
             console.log(res.data);
-            setTemp(res.data.main.temp - 273.15);
+            setTemp(res.data.main.temp -273.15 + 32);
             setIcon(res.data.weather[0].icon);
+            setMin(res.data.main.temp_min -273.15 +32);
+            setMax(res.data.main.temp_max -273.15 +32);
+            setDescription(res.data.weather[0].description);
+            setCountry(res.data.sys.country);
+            setShowMyComponent(true);
         })
           .catch((err) => {
             console.log(err)
@@ -29,22 +34,23 @@ const Weather = () => {
     }
 
     return (
-        <div className="container">
+        <div className="container my-4">
             <input 
                 type="text" 
                 value={city} 
                 onChange={(e) => setCity(e.target.value)}
-                placeholder="Cityname" 
+                placeholder="Cityname" className="mx-1 p-1" 
             />
             <input 
                 type="text" 
                 value={country} 
                 onChange={(e) => setCountry(e.target.value)}
-                placeholder="Countryname" 
+                placeholder="Countryname" className="mx-2 p-1"
             />
             <button 
                 onClick={() => getWeatherData(city, country)} 
-                className="btn btn-primary">
+                className="btn btn-primary" style={{backgroundColor: "#51456a", fontWeight:"bold",
+                fontSize:20, border:0}}>
                 Get Weather
             </button>
 
@@ -59,9 +65,12 @@ const Weather = () => {
                      style={{width: 200, height: 200}}
                     />
                 </div>
-                {temp ? <h1>{Math.floor(temp)}℃</h1> : null}
-                <h4 className="my-4">Min: <span>{Math.floor(min)}℃</span></h4>
-                {/* ℉ */}
+                {temp ? <h1>{Math.floor(temp)}℉</h1> : null}
+                <h4 className="my-4">Min: <span>{Math.floor(min)}℉</span>
+                <span className="mx-3"> | </span> Max:<span>{Math.floor(max)}℉</span>                
+                </h4>
+                
+                <h1>{description}</h1>
             </div>
         </div>
     )
